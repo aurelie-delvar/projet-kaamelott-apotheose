6ff23473717c5a35bd6f70e55d9c5965e5e4a77a
aurelie-delvar <aurelie.delvar@gmail.com>
2023-04-04 19:26:17 +0200

    Pagination is on its way

--- src/Repository/QuoteRepository.php
+++ src/Repository/QuoteRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Quote;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Quote>
@@ -52,6 +53,50 @@ class QuoteRepository extends ServiceEntityRepository
         $quoteArray = $result->fetchAssociative();
 
         return $quoteArray;
+    }
+
+    /**
+     * A try to paginate the quote's page as it's far too long for now.
+     *
+     * @param int $page the page you're on.
+     * @param int $limit the number of quotes you want on one page. If nothing is indicated, it will be 10.
+     * 
+     * @return array
+     */
+    public function findQuotesPaginated(int $page, int $limit = 10): array
+    {
+        // with abs, limit is always positive (absolute)
+        $limit = abs($limit);
+
+        // initialisation of the results
+        $result = [];
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+            ->select('q') // "q" stands for "quotes"
+            ->from('App\Entity\Quote', 'q')
+            ->setMaxResults($limit) // we want 10 results by page
+            ->setFirstResult(($page * $limit) - $limit); // the first result in the current page
+
+        // we tell Doctrine we need to pagine with Paginator, and we pass our query
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        // we check if we really have datas
+        if(empty($data)) {
+            return $result; 
+        }
+
+        // we need to know the number of pages we generate, ceil round up the result
+        $pages = ceil($paginator->count() / $limit);
+
+        // we fill the array
+        $result['data'] = $data;
+        $result['pages'] = $pages;
+        $result['page'] = $page;
+        $result['limit'] = $limit;
+
+        // don't forget we return an array!
+        return $result;
     }
 
 //    /**
