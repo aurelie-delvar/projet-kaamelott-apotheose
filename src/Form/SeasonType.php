cf892e07669f04a012bf44b7c8b56b95f6561da0
Angèle LEGALL <angie301@hotmail.fr>
2023-04-05 12:27:51 +0200

    SeasonController method delete : done, SeasonController all methods : done

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Season;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,6 +21,9 @@ class SeasonType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Season::class,
+            'attr' =>[
+                'novalidate' => 'novalidate'
+            ]
         ]);
     }
 }
