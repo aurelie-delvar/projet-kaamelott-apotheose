066925f0635cb4a956d1981334a4d109177d52db
EdithBERNARD <edith.bernard80@gmail.com>
2023-04-07 16:17:50 +0200

    modification of season/id read.html.twig

--- src/Controller/Frontoffice/SeasonController.php
+++ src/Controller/Frontoffice/SeasonController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller\Frontoffice;
 
-use App\Repository\EpisodeRepository;
 use App\Repository\SeasonRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EpisodeRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SeasonController extends AbstractController
 {
     /**
      * @Route("/livre/{id}", name="app_frontoffice_season_read", requirements={"id":"\d+"})
      */
-    public function read($id, SeasonRepository $seasonRepository): Response 
+    public function read($id, SeasonRepository $seasonRepository, Request $request, PaginatorInterface $paginator, EpisodeRepository $episodeRepository): Response 
     {
         $season = $seasonRepository->find($id);
-
+ 
         return $this->render('frontoffice/season/read.html.twig', [
             'season' => $season,
         ]);
