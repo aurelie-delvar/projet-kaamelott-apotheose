42a99d8dff02addca207b984f6b31ade126d7be4
CelineCHANAVAT <celine.chanavat@orange.fr>
2023-04-07 17:52:37 +0200

    navbar backoffice ongoing

---
+++ src/Controller/Backoffice/SearchController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Backoffice;
+
+use App\Repository\QuoteRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+   
+
+    // TODO : route search : doit afficher un résultat de recherche.
+    /**
+     * résultat de recherche
+     *
+     * @Route("/backoffice/search",name="app_backoffice_search", methods={"GET", "POST"})
+     * 
+     * @return Response
+     */
+    public function search(Request $request, QuoteRepository $quoteRepository, PaginatorInterface $paginator): Response
+    {
+
+    
+      
+        $words = $request->request->get("searchBack");
+
+        
+ 
+    
+        // Paginate the results of the query
+        $pagination = $paginator->paginate(
+        // Doctrine Query, not results
+        $quoteRepository->querySearchBack($words),
+        // Define the page parameter
+        $request->query->getInt('page', 10),
+        // Items per page
+        4
+    );
+
+
+
+    return $this->render('backoffice/search/index.html.twig', [
+        'pagination' => $pagination,
+    ]);
+
+        
+    }
+    
+
+}
--- src/Controller/Frontoffice/HomeController.php
+++ src/Controller/Frontoffice/HomeController.php
@@ -60,4 +60,5 @@ class HomeController extends AbstractController
         ]);
     }
 
+    
 }
