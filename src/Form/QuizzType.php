410413878d5045addfcf9acb871bca72066b1c1c
aurelie-delvar <aurelie.delvar@gmail.com>
2023-04-07 13:59:03 +0200

    Some little things were missing in the QUizz entity

--- src/Form/QuizzType.php
+++ src/Form/QuizzType.php
@@ -6,13 +6,16 @@ use App\Entity\Quizz;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class QuizzType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class,[
+                'label' => 'Titre du quizz'
+            ])
         ;
     }
 
