b85eb62c66d12e064e9963921ad3a414d2b8f83d
EdithBERNARD <edith.bernard80@gmail.com>
2023-04-11 17:37:17 +0200

    testing favorites

--- src/Controller/Frontoffice/QuoteController.php
+++ src/Controller/Frontoffice/QuoteController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller\Frontoffice;
 
+use App\Entity\User;
+use App\Entity\Favorite;
 use App\Repository\QuoteRepository;
+use App\Repository\FavoriteRepository;
+use Doctrine\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class QuoteController extends AbstractController
 {
@@ -26,5 +32,36 @@ class QuoteController extends AbstractController
         return $this->render('frontoffice/quote/index.html.twig', [
             "pagination" => $pagination,
         ]);
+    }
+
+    /**
+     * @Route("/favoris/{id}", name="app_frontoffice_quotes_addFavorite", requirements={"id" : "\d+"})
+     */
+    public function addFavorite(QuoteRepository $quoteRepository, $id, FavoriteRepository $favoriteRepository, Request $request, UserInterface $user, ObjectManager $manager): Response
+    {
+
+        $quote = $quoteRepository->find($id);
+
+        if(!$quote) {
+            throw new NotFoundHttpException("Pas de citation trouvée");
+        }
+
+        
+        $user->getUserIdentifier();
+        
+        $newFavorite = new Favorite();
+        $newFavorite->setUser($user);
+        $newFavorite->setQuote($quote);
+
+        $manager->persist($newFavorite);
+
+        $manager->flush();
+
+        // dd($newFavorite);
+
+        // $essai = $quote->addFavorite($newFavorite);
+
+
+        return $this->redirectToRoute($request->attributes->get('_route'));
     }
 }
