27e970486e7d213b3dc2d38c84634a41a3383542
Angèle LEGALL <angie301@hotmail.fr>
2023-04-13 09:31:04 +0200

    code cleaning for Api Quizz

--- public/main.js
+++ public/main.js
@@ -15,96 +15,16 @@ let resultat = document.getElementById("resultat");
 var questions = {};
 
 
-/*function showQuestion(index){
-
-    const questionList = appContainer;
-
-    const popup = document.createElement('div');
-    popup.classList.add('question');
-
-    const questionElement = document.createElement('div');
-    
-    const titleElement = document.createElement('h4');
-    titleElement.textContent = question.title;
-
-    const answersElement = document.createElement('div');
-    answersElement.id = 'answers';
-    answersElement.classList.add('answers');
-
-    const answer1Element = document.createElement('button');
-    answer1Element.classList.add('answer');
-    answer1Element.textContent = question.answer1;
-
-    const answer2Element = document.createElement('button');
-    answer2Element.classList.add('answer');
-    answer2Element.textContent = question.answer2;
-
-    const answer3Element = document.createElement('button');
-    answer3Element.classList.add('answer');
-    answer3Element.textContent = question.answer3;
-
-    const answer4Element = document.createElement('button');
-    answer4Element.classList.add('answer');    
-    answer4Element.textContent = question.answer4;
-    
-    popup.append(questionElement, answersElement);
-    questionElement.append(titleElement);
-    answersElement.append(answer1Element, answer2Element, answer3Element, answer4Element);
-
-    questionList.append(popup);
-
-
-    
-    answer1Element.addEventListener("click", function(){
-        if(question.answer1 == question.goodAnswer){
-            score += 1;
-            console.log('bonne réponse'+ score);
-        }
-        answeredQuestions += 1;
-        console.log(answeredQuestions);
-    }); 
-    
-    answer2Element.addEventListener("click", function(){
-        if(question.goodAnswer === question.answer2){
-            score += 1;
-            console.log('bonne réponse'+ score);
-        }
-        answeredQuestions += 1;
-        console.log(answeredQuestions);
-    }); 
-
-    answer3Element.addEventListener("click", function(){
-        if(question.goodAnswer === question.answer3){
-            score += 1;
-            console.log('bonne réponse'+ score);
-        }
-        answeredQuestions += 1;
-        console.log(answeredQuestions);
-    }); 
-    answer4Element.addEventListener("click", function(){
-        if(question.goodAnswer === question.answer4){
-            score += 1;
-            console.log('bonne réponse'+ score);
-        }
-        answeredQuestions += 1;
-        console.log(answeredQuestions);
-    }); 
-
-
-    
-}*/
-
 async function getQuestions() {
 
     const req = await fetch(`${urlQuizz}`);
     
     const quizz = await req.json();
-   const questions = quizz.questions;
+    const questions = quizz.questions;
   
    return questions;
-    
- //showQuestion(questions[0]);
-  }
+
+}
 
 function next(){
 	
@@ -150,9 +70,9 @@ function compare  (a,b){
 }
 
 async function showQuestion(index){
-	/*nbBonnereponse=0;*/
+
 	current_index = index;
-   let questions = await this.getQuestions();
+    let questions = await this.getQuestions();
     console.log (questions);
     
     console.log(questions[index].title);
