37a4221d6a22c8059ef63c614960330fd6cf8e7c
EdithBERNARD <edith.bernard80@gmail.com>
2023-04-13 22:16:50 +0200

    favorite is working add and remove without quote-card -code have to be cleaned

--- src/Controller/Frontoffice/QuoteController.php
+++ src/Controller/Frontoffice/QuoteController.php
@@ -6,10 +6,11 @@ use App\Entity\User;
 use App\Entity\Favorite;
 use App\Repository\QuoteRepository;
 use App\Repository\FavoriteRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -21,17 +22,19 @@ class QuoteController extends AbstractController
     /**
      * @Route("/citations", name="app_frontoffice_quotes_browse")
      */
-    public function browse(QuoteRepository $quoteRepository, Request $request, PaginatorInterface $paginator): Response
+    public function browse(QuoteRepository $quoteRepository, Request $request, Security $security, PaginatorInterface $paginator): Response
     {
-
+        // je récupère le User s'il est connecté
+        $user = $security->getUser();
         $pagination = $paginator->paginate(
             $quoteRepository->paginationQuery(),
             $request->query->get('page', 1),
             10,
         );
-
+// dd($pagination);
         return $this->render('frontoffice/quote/index.html.twig', [
             "pagination" => $pagination,
+            "user" => $user,
         ]);
     }
 
