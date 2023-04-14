25eb45f6239400e7a816f2ea63c8bbd4322e05a1
Angèle LEGALL <angie301@hotmail.fr>
2023-04-14 11:37:19 +0200

    try url

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,15 @@
 
 // I retrieve the url of my current page
 const url= document.location.href;
-
+console.log(url);
 // I remove the url from my page and I only keep the last information of the url
 const idPage = url.substring(url.lastIndexOf( "/" )+1 ); 
-
+console.log(idPage);
 const urlApi = document.location.origin + '/api/quizz/'; 
-
+console.log(urlApi);
 // I concatenate the url of the api with the id of my page
 const urlQuizz = urlApi + idPage;
-
+console.log(urlQuizz);
 //console.log(urlQuizz);
 
 function initApp(){
