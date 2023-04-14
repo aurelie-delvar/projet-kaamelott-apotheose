4f379e8ed889e4f2540ed942f663391328899ca9
Angèle LEGALL <angie301@hotmail.fr>
2023-04-14 13:24:43 +0200

    try test url api

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,17 @@
 const url= document.location.href;
 // I remove the url from my page and I only keep the last information of the url
 const idPage = url.substring(url.lastIndexOf( "/" )+1 );
-// I concatenate the url of the api with the id of my page
-const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
 
-//console.log(urlQuizz);
+const urlProtocole = document.location.protocol;
+console.log(urlProtocole);
+const urlHost = document.location.host;
+const urlAddApi = "/api/quizz/";
+const urlApi = urlProtocole + "//" + urlHost + urlAddApi; 
+console.log(urlApi);
+// I concatenate the url of the api with the id of my page
+//const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
+const urlQuizz = urlApi + idPage;
+console.log(urlQuizz);
 
 function initApp(){
    
