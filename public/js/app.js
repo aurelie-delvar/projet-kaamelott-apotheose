d17321d77ae948630f065cfa053210fa5e5c4f37
Angèle LEGALL <angie301@hotmail.fr>
2023-04-14 11:30:42 +0200

    modify url api

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,14 @@
 
 // I retrieve the url of my current page
 const url= document.location.href;
+
 // I remove the url from my page and I only keep the last information of the url
-const idPage = url.substring(url.lastIndexOf( "/" )+1 );
+const idPage = url.substring(url.lastIndexOf( "/" )+1 ); 
+
+const urlApi = document.location.origin + '/api/quizz/'; 
+
 // I concatenate the url of the api with the id of my page
-const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
+const urlQuizz = urlApi + idPage;
 
 //console.log(urlQuizz);
 
