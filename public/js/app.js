5086b192a23591b43750cf718a3c49af874b731c
Angèle LEGALL <angie301@hotmail.fr>
2023-04-18 09:34:31 +0200

    modifiy url Api Quizz

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,16 @@
 
 // I retrieve the url of my current page
 const url= document.location.href;
+console.log(url);
+const newUrl = url.replace("quizz/1","");
+console.log(newUrl);
 // I remove the url from my page and I only keep the last information of the url
 const idPage = url.substring(url.lastIndexOf( "/" )+1 );
-// I concatenate the url of the api with the id of my page
-const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
 
-//console.log(urlQuizz);
+// I concatenate the url of the api with the id of my page
+//const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
+const urlQuizz = newUrl + "api/quizz/" + idPage;
+console.log(urlQuizz);
 
 function initApp(){
    
