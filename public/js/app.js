bc9e4bb4932387aac00d40c90b713932de5921d5
Angèle LEGALL <angie301@hotmail.fr>
2023-04-18 09:11:55 +0200

    url api modify

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,16 @@
 
 // I retrieve the url of my current page
 const url= document.location.href;
+console.log(url);
+const newUrl = url.replace("quizz/1","");
+console.log(newUrl);
 // I remove the url from my page and I only keep the last information of the url
 const idPage = url.substring(url.lastIndexOf( "/" )+1 );
 
-const urlProtocole = document.location.protocol;
-console.log(urlProtocole);
-const urlHost = document.location.host;
-const urlAddApi = "/api/quizz/";
-const urlApi = urlProtocole + "//" + urlHost + urlAddApi; 
-console.log(urlApi);
+
 // I concatenate the url of the api with the id of my page
 //const urlQuizz = 'http://localhost:8000/api/quizz/' + idPage;
-const urlQuizz = urlApi + idPage;
+const urlQuizz = newUrl + "api/quizz/" + idPage;
 console.log(urlQuizz);
 
 function initApp(){
