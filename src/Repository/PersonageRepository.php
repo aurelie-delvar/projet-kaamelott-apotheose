88d8d76b2eb899d5c00ecdd666a0092ad906eac7
EdithBERNARD <edith.bernard80@gmail.com>
2023-04-20 14:40:03 +0200

    repository check

--- src/Repository/PersonageRepository.php
+++ src/Repository/PersonageRepository.php
@@ -39,18 +39,18 @@ class PersonageRepository extends ServiceEntityRepository
         }
     }
 
-   /**
+    /**
     * Trying to paginate with the paginator bundle which require a Doctrine query.
     * We want a query in order for the paginate method of the paginator
     * This is for the front & back offices.
     */
-   public function paginationQuery()
-   {
+    public function paginationQuery()
+    {
        return $this->createQueryBuilder('p')
            ->orderBy('p.creditOrder, p.name', 'ASC')
            ->getQuery()
        ;
-   }
+    }
 
 
 }
